66e21d13c3049e45414182fa3a0baced24c60a5b
Halry <halrybhalodia17@gmail.com>
2021-03-26 15:55:26 +0530

    second version

--- app/src/main/java/com/example/project/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/project/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.project.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,18 +15,51 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.project.R;
+import com.example.project.login1;
+import com.example.project.register1;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private Button update;
+    private Button useful_links;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-
-       
+        super.onCreate(savedInstanceState);
         return root;
+
+        update = findViewById(R.id.button);
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateActivity();
+            }
+        });
+        useful_links = findViewById(R.id.button4);
+
+        useful_links.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                useful_linksActivity();
+            }
+        });
+    }
+
+
+}
+    private void updateActivity() {
+        Intent intent = new Intent(this, update.class);
+        startActivity(intent);
+    }
+
+    private void useful_linksActivity() {
+        Intent intent = new Intent(this, useful_links.class);
+        startActivity(intent);
     }
 }
+
